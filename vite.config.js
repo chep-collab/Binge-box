60e11cbdc6c311dcc397cb2de96849a2b891057f
Mercy chepngeno <mercychepngeno582@gmail.com>
2025-02-19 19:01:22 +0300

    Configure deployment to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Binge-box/', // Replace with your repository name
   plugins: [react()]
 });
